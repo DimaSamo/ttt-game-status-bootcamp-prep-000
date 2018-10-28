3cbd7986e2dad6d1bdc0244fd70ed952a248cb47
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-28 16:50:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combination|
-    if (board[combination[0]] == "X" || board[combination[0]] == "O") && board[combination[0]] == board[combination[1]] == board[combination[2]]
+    if (board[combination[0]] == "X" || board[combination[0]] == "O") && (board[combination[0]] == board[combination[1]]) && (board[combination[1]] == board[combination[2]])
     end
   end
 end
